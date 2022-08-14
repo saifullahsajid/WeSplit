c1d5d13a0d07bc02564ad01c2c6d97093bb7fcf0
Saif Ullah Sajid <saifbinsajid7@gmail.com>
2022-08-14 12:14:54 +0500

    Implemented tip amount calculation

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,9 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: "USD"))
+                }
+            }
+            .navigationTitle("Tip Calculation")
+            .navigationBarTitleDisplayMode(.large)
+        }
     }
 }
 
